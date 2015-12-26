b284e7d7fa7716a5586b01767e152d09a069e288
igor <igor@73d55a97-b415-f84c-ab56-998100a9093c>
2015-12-26 13:40:07 +0000

    git-svn-id: https://localhost:8443/svn/ig_base_testdevelop/igsolutions/trunk@273 73d55a97-b415-f84c-ab56-998100a9093c

---
+++ syncsolutons.bat
@@ -0,0 +1,15 @@
+
+echo.
+echo Synchronizing solution files FROM their original locations in ..\solutions...
+echo.
+
+copy /y IGLib.sln ..\iglib
+echo.
+copy /y ShellDev.sln ..\shelldev
+echo.
+copy /y ShellDevAll.sln ..\shelldev
+
+echo. 
+echo.
+echo ... synchronization of solutions done.
+echo.
