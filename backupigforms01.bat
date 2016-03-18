b94fe61a8de341d119c2665917237a77f403e750
igor <igor@73d55a97-b415-f84c-ab56-998100a9093c>
2016-03-18 01:27:55 +0000

    git-svn-id: https://localhost:8443/svn/ig_base_testdevelop/igsolutions/trunk@285 73d55a97-b415-f84c-ab56-998100a9093c

---
+++ backupigforms01.bat
@@ -0,0 +1,31 @@
+
+
+rem echo off
+
+echo.
+echo Copying IGForms project director for temp. backup
+echo to prevent loss of form due to GUI designer bugs...
+echo.
+
+set sourcedir=..\iglib\igforms
+set targetdir= .\Backup\igforms01\
+
+
+echo Source directory: %sourcedir%
+echo Target directory: %targetdir%
+
+
+md %targetdir%
+rd /s /q %targetdir%
+md %targetdir%
+
+
+echo.
+echo.
+echo.
+
+xcopy /y /e /i %sourcedir% %targetdir%
+
+rem igform.csproj
+
+
